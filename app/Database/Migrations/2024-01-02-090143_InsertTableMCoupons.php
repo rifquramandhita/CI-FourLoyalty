7e3ff85b983925d9038ec3c922e638a6b0764782
rifquramandhita <rifquramandhita@gmail.com>
2024-01-02 17:27:35 +0700

    feat:add migration InsertTableMCoupons

---
+++ app/Database/Migrations/2024-01-02-090143_InsertTableMCoupons.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class InsertTableMCoupons extends Migration
+{
+    public function up()
+    {
+        $data1 = [
+            'title' => 'Discount 75%',
+            'description' => '*snk',
+            'img_path' => 'uploads/img/default.png',
+            'is_active' => '1',
+        ];
+        $data2 = [
+            'title' => 'Discount 50%',
+            'description' => '*snk',
+            'img_path' => 'uploads/img/default.png',
+            'is_active' => '1',
+        ];
+        $data3 = [
+            'title' => 'Discount 25%',
+            'description' => '*snk',
+            'img_path' => 'uploads/img/default.png',
+            'is_active' => '1',
+        ];
+        $data4 = [
+            'title' => 'Discount 15%',
+            'description' => '*snk',
+            'img_path' => 'uploads/img/default.png',
+            'is_active' => '1',
+        ];
+        $data5 = [
+            'title' => 'Discount 5%',
+            'description' => '*snk',
+            'img_path' => 'uploads/img/default.png',
+            'is_active' => '1',
+        ];
+        $this->db->table('m_coupons')->insert($data1);
+        $this->db->table('m_coupons')->insert($data2);
+        $this->db->table('m_coupons')->insert($data3);
+        $this->db->table('m_coupons')->insert($data4);
+        $this->db->table('m_coupons')->insert($data5);
+    }
+
+    public function down()
+    {
+        //
+    }
+}
